7525f291553397b1360b37af15ae4a7936ca7c7b
Kevin Floyd <62043739+Keviwevii@users.noreply.github.com>
2024-10-13 21:38:20 -0500

    adding the last code for chapter 4

---
+++ chapter 4/buffet.py
@@ -0,0 +1,12 @@
+buffet_food = ("fish", "chicken", "veggies", "tofu", "noodles")
+
+print("This is the original menu: ")
+for food in buffet_food:
+    print(food)
+
+new_buffet_food = ("turkey", "chicken", "salad", "tofu", "noodles")
+
+print("\nThis is the adjusted menu: ")
+for food in new_buffet_food:
+    print(food)
+
