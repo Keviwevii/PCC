bfde66383c63e66ee0518bb6c711cd07146fa68b
Kevin Floyd <62043739+Keviwevii@users.noreply.github.com>
2024-10-22 17:59:02 -0500

    adding conditional tests

---
+++ chapter 5/conditional_tests.py
@@ -0,0 +1,39 @@
+weather = "sunny"
+print("Is the weather = sunny? I think that is is not!")
+print(weather == 'rainy') #False
+
+color = 'red'
+print("\nIs Winnie the Pooh wearing his red shirt today? It's his signature color!")
+print(color == 'red') #True
+
+lamp = 1
+print("\nIs the lamp on? It looks like it is!")
+print(lamp == 1) #True
+
+hand_exercises = 'finished'
+print("\nHave you finished your hand exercises today? I think you did right?")
+print(hand_exercises == 'finished') #True
+
+work_day = 'long'
+print("\nWas the work day long today?")
+print(work_day == 'long') #True
+
+candle = 'burning'
+print("\nIs that candle burning? Looks like it is.")
+print(candle == 'burning') #True
+
+tv_status = "off"
+print("\nIs the tv off?")
+print(tv_status == "on") #False
+
+feeling = "sad"
+print("\nAre you feeling sad?")
+print(feeling == "happy") #False
+
+roof = "not raised"
+print("\nIs the roof raised?")
+print(roof == "raised") #False
+
+mode = "not focused"
+print("\nIs focused mode on?")
+print(mode == "focused") #False
---
+++ chapter 5/more_conditional_tests.py
@@ -0,0 +1,11 @@
+cheese = "yellow"
+print("\nIs that cheese green?")
+print(cheese != 'yellow')
+print("\nIsn't cheese usually yellow?")
+print(cheese == "yellow")
+
+name = "Kevin"
+print("Is your name in lowercase victor?")
+print( name.lower() == "victor")
+print("Is your name in lowercase kevin?")
+print(name.lower() == "kevin")
