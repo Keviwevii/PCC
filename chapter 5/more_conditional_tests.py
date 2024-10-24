837e47e1e22fbce1c2bbe76c33ca11599dfebe72
Kevin Floyd <62043739+Keviwevii@users.noreply.github.com>
2024-10-23 21:22:56 -0500

    adding more code from chapter 5

--- chapter 5/more_conditional_tests.py
+++ chapter 5/more_conditional_tests.py
@@ -5,7 +5,33 @@ print("\nIsn't cheese usually yellow?")
 print(cheese == "yellow")
 
 name = "Kevin"
-print("Is your name in lowercase victor?")
+print("\nIs your name in lowercase victor?")
 print( name.lower() == "victor")
-print("Is your name in lowercase kevin?")
+print("\nIs your name in lowercase kevin?")
 print(name.lower() == "kevin")
+
+favorite_number = 8
+print("\nIs your favorite number greater than 10?")
+print(favorite_number > 10)
+print("\nIs your favorite number greater than 6?")
+print(favorite_number > 6)
+
+favorite_color = "red"
+favorite_car = "honda"
+
+print("\nDo you like the color blue or BMW cars?")
+print(favorite_color == "blue" or favorite_car == "BMW")
+print("\nDo you like the color red and honda cars?")
+print(favorite_color == "red" and favorite_car == "honda")
+
+color_list = ["red", "blue", "purple"]
+print("\nIs blue in our color list?")
+print("blue" in color_list)
+print("\nIs indigo in our color list?")
+print("indigo" in color_list)
+
+print("\nIs red not in our list?")
+print("red" not in color_list)
+
+print("\nIs magenta not in our list?")
+print("indigo" not in color_list)
