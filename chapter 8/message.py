dec9493324e58dbb2f1f97d6863229fc240e3cac
Kevin Floyd <62043739+Keviwevii@users.noreply.github.com>
2024-11-18 11:40:03 -0600

    adding code from chapter 8

---
+++ chapter 8/message.py
@@ -0,0 +1,6 @@
+def display_message():
+    """Display what the chapter is about"""
+    print("In this chapter I am learning about functions.")
+
+
+display_message()
---
+++ chapter 8/t-shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size,text):
+    print(f"The size of the shirt is {size} and the text will say: {text}")
+
+make_shirt("medium", "Hello World")
+make_shirt(text = "Goodbye cruel world!", size = "XL")
